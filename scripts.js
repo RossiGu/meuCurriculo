fa33e988825eb7b84dc4448d2a26cbfb5d556c76
RossiGu <gustavo.2017316329@gmail.com>
2023-04-23 20:11:41 -0300

    commit

--- scripts.js
+++ scripts.js
@@ -2,4 +2,28 @@ let botton = document.getElementById("bottonFixed");
 
 botton.addEventListener("click", function () {
     window.scrollTo(0, 0);
+}); // Estava sem JQuery
+
+$("#professional").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#performance").offset().top
+    }, 1000);
+});
+
+$("#about").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#aboutme").offset().top
+    }, 1000);
+});
+
+$("#formation1").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#formation").offset().top
+    }, 1000);
+});
+
+$("#tools").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#skills").offset().top
+    }, 1000);
 });
